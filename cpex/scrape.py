68707036073f82039afdcbb1accc9f1e672e3aed
casimp <c.a.simpson01@gmail.com>
2019-08-30 13:31:47 +0100

    Started to create a shell for data scrape.

--- cpex/scrape.py
+++ cpex/scrape.py
@@ -2,3 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import h5py
 import os
+
+class scrape_odb(lattice_calc):
+    def __init__(self, fpath):
+        self.fpath = fpath
+        
+        
+
+class lattice_calc():
+    def extract_lattice(self):
+        pass
+    
+    def calc_lattice_tensor(self):
+        pass
