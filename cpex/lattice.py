4b054aa7a251c96cc085f80268c6f1cd1481f9b9
casimp <c.a.simpson01@gmail.com>
2019-09-03 18:29:38 +0100

    Added subset plotting functionality.

--- cpex/lattice.py
+++ cpex/lattice.py
@@ -21,12 +21,16 @@ class Load():
         self.N = data['N']
         self.num_frames = data['num_frames']
         self.num_grains = data['num_grains']
-        
+        try:
+            self.t = data['time']
+        except KeyError:
+            print('time not saved in file, creating zero array')
+            self.t = np.zeros((self.num_frames, ))
         self.rot[:, :,0] = self.rot[:, :,1]
         
         
     
-    def plot(self, y='stress', x='frame', idx=1):
+    def plot(self, y='stress', x='frame', idx=1, alpha=0.2):
         
         #Need tp average over volume.
         
@@ -35,18 +39,41 @@ class Load():
              'lattice':self.lat,
              'rot':self.rot - self.rot[:,:, 0][:, :, None]}
         
-        dx = {'time':np.arange(self.num_frames),
+        dx = {'time':self.t,
              'stress':np.nanmean(self.s[idx], axis=0),
              'strain':np.nanmean(self.e[idx], axis=0),
              'frame':np.arange(self.num_frames),
              'lattice':np.nanmean(self.lat[idx], axis=0)}
         
         
-        plt.plot(dx[x], dy[y][idx].T, color='k', alpha=0.2)
+        plt.plot(dx[x], dy[y][idx].T, color='k', alpha=alpha)
         plt.plot(dx[x],np.nanmean(dy[y][idx], axis=0), color='r')
         plt.ylabel(y)
         plt.xlabel(x)
 
+
+    def plot_subset(self, grain_list, y='stress', x='frame', idx=1, alpha=0.2, 
+                    random=None):
+        
+        if random != None:
+            grain_list = np.random.choice(self.num_grains, random)
+        
+        dy = {'strain':self.e[:,grain_list],
+             'stress':self.s[:,grain_list],
+             'lattice':self.lat[:,grain_list],
+             'rot':(self.rot - self.rot[:,:, 0][:, :, None])[:,grain_list]}
+        
+        dx = {'time':self.t,
+             'stress':np.nanmean(self.s[idx], axis=0),
+             'strain':np.nanmean(self.e[idx], axis=0),
+             'frame':np.arange(self.num_frames),
+             'lattice':np.nanmean(self.lat[idx], axis=0)}
+        
+        
+        plt.plot(dx[x], dy[y][idx].T, color='k', alpha=alpha)
+        plt.plot(dx[x],np.nanmean(dy[y][idx], axis=0), color='r')
+        plt.ylabel(y)
+        plt.xlabel(x)
 
     def extract_lattice(self):
         pass
